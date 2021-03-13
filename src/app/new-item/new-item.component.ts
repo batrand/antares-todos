28af09f86717bb36d8a02a9f625c7992331fb4df
Ba Tran <ba.trand@outlook.com>
2021-03-13 21:15:18 +1100

    Add item working

--- src/app/new-item/new-item.component.ts
+++ src/app/new-item/new-item.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { TodoItem } from 'src/models/TodoItem';
 import { TodoStatus, TodoStatusLabels } from 'src/models/TodoStatus';
+import { TodoService } from 'src/services/TodoService';
 
 @Component({
   selector: 'new-item',
@@ -11,20 +13,27 @@ export class NewItemComponent implements OnInit {
   public statusLabels = TodoStatusLabels;
   public statusTypes = Object.values(TodoStatus);
 
-  public newTitle: String = "";
-  public newStatus: String = this.statusLabels[TodoStatus.NotStarted]; // Haven't figured out how to do 2way enum binding
+  public newTitle: string = "";
+  public newStatus: string = this.statusLabels[TodoStatus.NotStarted]; // Haven't figured out how to do 2way enum binding
   public newDue: Date = new Date();
 
-  public datetimeControl = new FormControl()
-  public minDateTime = Date() // cannot create Todos in the past
+  public datetimeControl = new FormControl();
+  public minDateTime = Date(); // cannot create Todos in the past
 
-  constructor() { }
+  private todoService: TodoService;
+
+  constructor(todoService: TodoService) {
+    this.todoService = todoService;
+  }
 
   ngOnInit(): void {
   }
 
   addItem() {
     console.log(`Called add item, ${this.newTitle} ${this.newStatus} ${this.newDue}`)
+    let newItem = new TodoItem(this.newTitle, this.newDue, this.newStatus as TodoStatus);
+    console.log(`New item: ${newItem}`);
+    this.todoService.add(newItem);
   }
 
 }
